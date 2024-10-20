1e40d7e0ea09ae82cda71fc62a98d7c5da305bb3
Susanlt <susanlt@me.com>
2024-10-20 12:19:04 -0700

    added photos

--- scripts/gallery.js
+++ scripts/gallery.js
@@ -1,19 +1,13 @@
 const photos = [
-  {
-    src: "images/amber-jeff-wedding1.jpg",
-    alt: "Backview of the bride dancing with her husband",
-    orientation: "horizontal",
-  },
+  
   {
     src: "images/amber-jeff-wedding2.jpg",
     alt: "Backview of the bride dancing with her husband looking at the camera over her shoulder",
     orientation: "horizontal",
-    span: 14,
-    fit: "cover",
-  }, // Using cover for artistic look
+  },
   {
     src: "images/amber-jeff-wedding3.jpg",
-    alt: "Backview of the bride dancing with her husband side-angle",
+    alt: "Sideview of the bride dancing with her husband",
     orientation: "vertical",
   },
   {
@@ -23,16 +17,286 @@ const photos = [
   },
   {
     src: "images/amber-jeff-wedding-flowers.jpg",
-    alt: "The bride gazing down at her bouquet of pink flowers",
+    alt: "The bride gazing down at her bouquet of pink amber-jeff-wedding-flowers",
     orientation: "vertical",
-    fit: "cover",
-  }, 
+  },
+  {
+    src: "images/amber-jeff-wedding-veil.jpg",
+    alt: "Bride and groom facing each other with the bride's veil over their heads. Pink flower bouquet in the foreground",
+    orientation: "vertical",
+  },
+  {
+    src: "images/amber-jeff-wedding-veil2.jpg",
+    alt: "Bride and groom facing each other with the bride's veil over their heads. Pink flower bouquet in the foreground",
+    orientation: "vertical",
+  },
+  {
+    src: "images/amber-jeff-wedding-hands.jpg",
+    alt: "A close-up shot of the bride's and groom's hands",
+    orientation: "horizontal",
+  },
+  {
+    src: "images/amber-jeff-wedding1.jpg",
+    alt: "Backview of the bride dancing with her husband",
+    orientation: "horizontal",
+  },
+  {
+    src: "images/annika-the-moviestar.jpg",
+    alt: "Portrait of woman dressed in a fur coat and pearls, looking over her left shoulder",
+    orientation: "vertical",
+  },
+  {
+    src: "images/becca-weber-bride1.jpg",
+    alt: "Portrait of a bride with dark hair, long curls, and tiara",
+    orientation: "vertical",
+  },
+  {
+    src: "images/becca-weber-bride2.jpg",
+    alt: "A black and white photo of a bride lounging on a chaise",
+    orientation: "horizontal",
+  },
+  {
+    src: "images/becca-weber-bride3.jpg",
+    alt: "A black and white portrait of a bride with dark hair, long curls, and tiara",
+    orientation: "horizontal",
+  },
+  {
+    src: "images/cannon-beach-child-kite.jpg",
+    alt: "A small child on the beach with a red kite flying overhead",
+    orientation: "horizontal",
+    fit: "contain",
+  },
+  {
+    src: "images/doug-arena-wedding-party.jpg",
+    alt: "Wedding party portrait in a church",
+    orientation: "horizontal",
+  },
+  {
+    src: "images/doug-arena-brides-ring.jpg",
+    alt: "Bride's hand surrounded by the veil",
+    orientation: "vertical",
+  },
+  {
+    src: "images/blonde-bride1.jpeg",
+    alt: "A blonde bride",
+    orientation: "vertical",
+  },
+  {
+    src: "images/jennys-7th-anniversary1.jpg",
+    alt: "Headshot of a brunette woman under a white umbrella, blocking the sun",
+    orientation: "vertical",
+  },
+  
+  {
+    src: "images/jennys-7th-anniversary3.jpg",
+    alt: "Headshot of a brunette woman under a white umbrella",
+    orientation: "vertical",
+  },
+  {
+    src: "images/julie-brunk.jpg",
+    alt: "A brunette woman dressed elegantly in black, posing with her back toward the camera and her head gazing to the ground",
+    orientation: "vertical",
+  },
+
+  {
+    src: "images/blonde-bride2.jpeg",
+    alt: "A blonde bride",
+    orientation: "vertical",
+  },
+  {
+    src: "images/amber-in-a-field.jpg",
+    alt: "A young woman in a field of tall golden grasses, her head turned away",
+    orientation: "vertical",
+  },
+  {
+    src: "images/jennys-7th-anniversary2.jpg",
+    alt: "Portrait of a brunette woman holding an opened umbrella to the side",
+    orientation: "vertical",
+  },
+  {
+    src: "images/kirk-jamie-marriage-certificate.jpg",
+    alt: "Hands of the bride and groom on the marriage certificate, in black and white",
+    orientation: "horizontal",
+  },
+  {
+    src: "images/kirk-wedding-band.jpg",
+    alt: "A close-up shot of a groom's hand on his hip, showing his wedding band",
+    orientation: "horizontal",
+  },
+  {
+    src: "images/amber-in-red3.jpeg",
+    alt: "A girl in red",
+    orientation: "vertical",
+  },
+  {
+    src: "images/ladies-lunch.jpg",
+    alt: "Four women around a table with trays of food and glasses of wine in hand",
+    orientation: "vertical",
+  },
+  {
+    src: "images/draggin-the-ave.jpg",
+    alt: "Black and white photo of young woman with straw hat and arm flexed showing tattoo of a bat and pumpkin",
+    orientation: "horizontal",
+  },
+  {
+    src: "images/amber-falling-off-fence.jpeg",
+    alt: "A girl falling backward off a white fence",
+    orientation: "vertical",
+  },
+  {
+    src: "images/amber-by-the-fence.jpeg",
+    alt: "A girl sitting on the ground, leaning against a white fence with a white barn in the background",
+    orientation: "vertical",
+  },
+
+  {
+    src: "images/amber-in-red1.jpeg",
+    alt: "A girl in red",
+    orientation: "vertical",
+  },
+
   {
     src: "images/amber-in-the-pool.jpeg",
-    alt: "Woman with short hair is in the pool leaning on edge looking into the camera",
+    alt: "Woman with short hair in a pool leaning on edge looking into the camera",
     orientation: "horizontal",
-    // fit: "cover",
-  }
+  },
+  {
+    src: "images/amber-in-the-pool2.jpeg",
+    alt: "Woman emerging from a pool and throwing her back while water creates a halo around her head",
+    orientation: "horizontal",
+  },
+  {
+    src: "images/playful-by-the-pool.jpeg",
+    alt: "The sillouette of two people posing by the pool",
+    orientation: "vertical",
+  },
+
+  {
+    src: "images/amber-and-sunflowers.jpeg",
+    alt: "Portrait of a women by a garden of sunflowers",
+    orientation: "horizontal",
+    // fit: "contain",
+  },
+  {
+    src: "images/amber-in-pink-and-white1.jpeg",
+    alt: "A woman wearing a white bustier and pink ruffled skirt, dressed for a wedding",
+    orientation: "vertical",
+  },
+  {
+    src: "images/amber-in-pink-and-white2.jpeg",
+    alt: "A woman wearing a white bustier and pink ruffled skirt, dressed for a wedding",
+    orientation: "vertical",
+  },
+  {
+    src: "images/amber-in-pink-and-white3.jpeg",
+    alt: "A woman wearing a white bustier and pink ruffled skirt, dressed for a wedding",
+    orientation: "vertical",
+  },
+  {
+    src: "images/bride-behind-the-scenes.jpeg",
+    alt: "",
+    orientation: "vertical",
+  },
+  {
+    src: "images/amber-in-red2.jpeg",
+    alt: "A girl in red",
+    orientation: "vertical",
+  },
+  
+  {
+    src: "images/bride-in-thought.jpeg",
+    alt: "",
+    orientation: "vertical",
+  },
+  {
+    src: "images/wedding-dress-and-shoes.jpeg",
+    alt: "",
+    orientation: "vertical",
+  },
+  {
+    src: "images/wedding-car.jpeg",
+    alt: "",
+    orientation: "horizontal",
+    fit: "contain",
+  },
+  {
+    src: "images/bride-in-the-garden.jpeg",
+    alt: "",
+    orientation: "vertical",
+  },
+  {
+    src: "images/bride-on-stairs.jpeg",
+    alt: "",
+    orientation: "vertical",
+  },
+  {
+    src: "images/bride-with-pearls-in-black-and-white.jpeg",
+    alt: "",
+    orientation: "vertical",
+  },
+  {
+    src: "images/over-the-shoulder-topview.jpeg",
+    alt: "",
+    orientation: "vertical",
+  },
+  {
+    src: "images/pregnant-woman.jpeg",
+    alt: "",
+    orientation: "vertical",
+  },
+  {
+    src: "images/chocolate-face.jpeg",
+    alt: "",
+    orientation: "horizontal",
+    fit: "contain",
+  },
+  {
+    src: "images/amber-with-blue-hair.jpeg",
+    alt: "",
+    orientation: "vertical",
+  },
+
+  {
+    src: "images/beercan-curlers1.jpeg",
+    alt: "",
+    orientation: "horizontal",
+    fit: "contain",
+  },
+
+  {
+    src: "images/beercan-curlers2.jpeg",
+    alt: "",
+    orientation: "vertical",
+  },
+
+  {
+    src: "images/yellow-umbrella1.jpeg",
+    alt: "",
+    orientation: "horizontal",
+    fit: "contain",
+  },
+
+  {
+    src: "images/yellow-umbrella2.jpeg",
+    alt: "",
+    orientation: "horizontal",
+    fit: "contain",
+  },
+  {
+    src: "images/yellow-umbrella3.jpeg",
+    alt: "",
+    orientation: "horizontal",
+    fit: "contain",
+  },
+
+  {
+    src: "images/yellow-umbrella4.jpeg",
+    alt: "",
+    orientation: "horizontal",
+    fit: "contain",
+  },
+
+
 ];
 
 const galleryContainer = document.getElementById("photo-gallery");
@@ -41,17 +305,12 @@ photos.forEach((photo, index) => {
   const item = document.createElement("div");
   item.className = `item ${photo.orientation} mosaic-item${index + 1}`;
 
-  // Apply custom grid-row span if it exists
-  if (photo.span) {
-    item.style.gridRow = `span ${photo.span}`;
-  }
-
   const img = document.createElement("img");
   img.src = photo.src;
   img.alt = photo.alt;
 
-  // Apply object-fit based on the 'fit' property or default to 'contain'
-  img.style.objectFit = photo.fit || "contain";
+  // Apply object-fit based on the orientation
+  img.style.objectFit = photo.fit || (photo.orientation === "horizontal" ? "cover" : "contain");
 
   item.appendChild(img);
   galleryContainer.appendChild(item);
